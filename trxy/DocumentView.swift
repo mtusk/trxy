438c653ddb9a9a2af16f1f9984a8686de7b6906e
Adam Anderson <adam.anderson@chrobinson.com>
2020-01-05 10:37:25 -0600

    Adds default document-based app template

---
+++ trxy/DocumentView.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentView.swift
+//  trxy
+//
+//  Created by Adam Anderson on 1/5/20.
+//  Copyright © 2020 mtusk. All rights reserved.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var document: UIDocument
+    var dismiss: () -> Void
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("File Name")
+                    .foregroundColor(.secondary)
+
+                Text(document.fileURL.lastPathComponent)
+            }
+
+            Button("Done", action: dismiss)
+        }
+    }
+}
