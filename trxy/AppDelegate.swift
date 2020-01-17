333b1e246a0f9e688c2b5a720efbbc77ba3514ac
Adam Anderson <adam.anderson@chrobinson.com>
2020-01-17 09:51:40 -0600

    Generates macOS document-based-app from template

---
+++ trxy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  trxy
+//
+//  Created by Adam Anderson on 1/17/20.
+//  Copyright © 2020 mtusk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
