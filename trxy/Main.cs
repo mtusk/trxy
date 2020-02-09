b0a8be68c269fbe75dc6d30757a1b7d06364c56b
Adam Anderson <adam.anderson@chrobinson.com>
2020-02-09 16:03:45 -0600

    Template

---
+++ trxy/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace trxy
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
